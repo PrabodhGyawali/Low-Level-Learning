f4d815435f463cf8ac47275e8181e02c8910d5ae
PrabodhGyawali <prabodh.gyawali2005@gmail.com>
2024-11-11 10:11:18 +0000

    ready to submit however, need to fix >>>

--- user/my_shell.c
+++ user/my_shell.c
@@ -18,26 +18,9 @@ int strin(char c, const char *str) {
   return 0;
 }
 
-/* Remove redir_l & redir_r from arguments */
-int remove_last_argument(char** arguments, int* numargs) {
-  /* Remove last arg from argument list */
-  if (arguments == 0) {
-    return -1;
-  }
-  
-  (*numargs)--;
-  arguments[*numargs] = 0;
-
-  return 0;
-}
-
 
 /* Test Cases
-  TODO: `exec failed:  grep     s`
-  TODO: Stress test `;`
-    - `echo hello | stressfs; cat < stressfs0
-  TODO: ls; echo hello > output; ls
-  TODO: cat < input.txt > output.txt
+  FIXME: wierd behavior of ">>> "
 */
 
 /* Read a line of characters from stdin. */
@@ -77,8 +60,8 @@ void run_command(char* buf, int nbuf, int* pcp) {
   char* file_name_l = 0;
   char* file_name_r = 0;
   /* Remove position of file_name from arguments array */
-  int file_name_l_pos = -1; 
-  int file_name_r_pos = -1;
+  int file_name_l_ind = -1; 
+  int file_name_r_ind = -1;
 
   int p[2];
   int pipe_cmd = 0;
@@ -129,10 +112,11 @@ void run_command(char* buf, int nbuf, int* pcp) {
       }
       break;
     }
+    
     /* Start a word */
     if (ws && strin(buf[i], " \t>|<;") == 0) {
       numargs++;
-      if (file_name_r_pos == numargs || file_name_l_pos == numargs) {
+      if (file_name_r_ind == (numargs - 1) || file_name_l_ind == (numargs - 1)) {
         // decrement numargs
         numargs--;
       } else {
@@ -155,7 +139,7 @@ void run_command(char* buf, int nbuf, int* pcp) {
     if (buf[i] == '<') {
         redirection_left = 1;
         if (we) {
-          buf[i] = '\0';
+          buf[we] = '\0';
           we = 0;
           ws = 1;
         } 
@@ -163,7 +147,6 @@ void run_command(char* buf, int nbuf, int* pcp) {
           fprintf(2, "my_shell: missing file for redirection\n");
           exit(0);
         }
-        continue;
     }
     /* Redirection right */
     if (buf[i] == '>') {
@@ -178,7 +161,6 @@ void run_command(char* buf, int nbuf, int* pcp) {
         fprintf(2, "my_shell: missing file for redirection\n");
         exit(0);
       }
-      continue;
     }
 
     // TODO: Fix errors with: `ls; echo hello > output; ls`, where file_name_r = `output; ls`
@@ -203,12 +185,14 @@ void run_command(char* buf, int nbuf, int* pcp) {
 
         file_name_l = &buf[j];
         if (we) {
-          file_name_r_pos = numargs - 1;
+          file_name_l_ind = numargs - 1;
+          we = 0;
+          ws = 1;
         }
-        file_name_r_pos = numargs + 1;
+        file_name_l_ind = numargs;
         
       } else if (redirection_right && file_name_r == 0) {
-        int j = i;
+        int j = i+1;
         while (strin(buf[j], " \t><|;\n") == 1)
         {
           if (strin(buf[j], "><|;\n") == 1) {
@@ -219,9 +203,11 @@ void run_command(char* buf, int nbuf, int* pcp) {
         }
         file_name_r = &buf[j];
         if (we) {
-          file_name_r_pos = numargs - 1;
+          file_name_r_ind = numargs - 1;
+          we = 0;
+          ws = 1;
         }
-        file_name_r_pos = numargs + 1;
+        file_name_r_ind = numargs;
       } else {
         if (buf[i] == ' ' || buf[i] == '\n') {
           buf[i] = '\0';
@@ -231,48 +217,52 @@ void run_command(char* buf, int nbuf, int* pcp) {
   }
 
   /* Testing */
-  printf("\n\nNum of args: %d\n", numargs);
-  for (int i = 0; i < numargs; i++)
-  {
-    printf("arguments[%d]: %s\n", i, arguments[i]);
-  }
-  printf("flags: pipe_cmd=%d, sequence_cmd=%d, numargs=%d, ws=%d, we=%d\n", pipe_cmd, sequence_cmd, numargs, ws, we);
-  printf("redir_l: %d\n", redirection_left);
-  printf("redir_r: %d\n", redirection_right);
-  printf("file_l: %s\n", file_name_l);
-  printf("file_r: %s\n", file_name_r);
-  printf("file_l_pos: %d\n", file_name_l_pos);
-  printf("file_r_pos: %d\n\n\n", file_name_r_pos);
+  // printf("\n\nNum of args: %d\n", numargs);
+  // for (int i = 0; i < numargs; i++)
+  // {
+  //   printf("arguments[%d]: %s\n", i, arguments[i]);
+  // }
+  // printf("flags: pipe_cmd=%d, sequence_cmd=%d, numargs=%d, ws=%d, we=%d\n", pipe_cmd, sequence_cmd, numargs, ws, we);
+  // printf("redir_l: %d\n", redirection_left);
+  // printf("redir_r: %d\n", redirection_right);
+  // printf("file_l: %s\n", file_name_l);
+  // printf("file_r: %s\n", file_name_r);
+  // printf("file_l_pos: %d\n", file_name_l_ind);
+  // printf("file_r_pos: %d\n\n\n", file_name_r_ind);
   /*
     Sequence command. Continue this command in a new process.
     Wait for it to complete and execute the command following ';'.
   */
   if (sequence_cmd) {
     sequence_cmd = 0;
-    // TODO: Add cd
 
     int pid = fork();
     if (pid < 0) {
       fprintf(2, "my_shell: fork failed\n");
       exit(1);
     } else if (pid == 0) {
+      /* Child process */
+      if (redirection_left) {
+        int redir_fd = open(file_name_l, O_RDONLY);
+        if (redir_fd < 0) {
+            fprintf(2, "my_shell: cannot open %s\n", file_name_l);
+            exit(1);
+        }
+        close(0);
+        if (dup(redir_fd) < 0) {
+          fprintf(2, "redirection dup error\n");
+          close(redir_fd);
+          exit(1);
+        }
+        close(redir_fd);
+      }
       if (redirection_right) {
         int redir_fd = open(file_name_r, O_CREATE | O_TRUNC | O_WRONLY);
         if(redir_fd < 0) {
-          fprintf(2, "my_shell: cannot open %s\n", file_name_r);
-          exit(1);
+            fprintf(2, "my_shell: cannot open %s\n", file_name_r);
+            exit(1);
         }
         close(1);
-        dup(redir_fd);
-        close(redir_fd);
-      }
-      if (redirection_left) {
-        int redir_fd = open(file_name_l, O_WRONLY);
-        if (redir_fd < 0) {
-          fprintf(2, "my_shell: cannot open %s\n", file_name_l);
-          exit(1);
-        }
-        close(0);
         dup(redir_fd);
         close(redir_fd);
       }
@@ -280,7 +270,7 @@ void run_command(char* buf, int nbuf, int* pcp) {
       exit(1);
     } else {
       wait(0);
-      printf("sequence returned, running: {%s}\n", sequence_pointer);
+      // printf("sequence returned, running: {%s}\n", sequence_pointer);
       run_command(sequence_pointer, nbuf - (sequence_pointer - buf), pcp);
     }
   }
@@ -303,9 +293,6 @@ void run_command(char* buf, int nbuf, int* pcp) {
       exit(1);
     }
     close(redir_fd);
-    exec(arguments[0], arguments);
-    fprintf(2, "exec failed: %s\n", arguments[0]);
-    exit(1);
   }
   if (redirection_right) {
     int redir_fd = open(file_name_r, O_CREATE | O_TRUNC | O_WRONLY);
@@ -316,9 +303,6 @@ void run_command(char* buf, int nbuf, int* pcp) {
     close(1);
     dup(redir_fd);
     close(redir_fd);
-    exec(arguments[0], arguments);
-    fprintf(2, "exec failed %s\n", arguments[0]);
-    exit(1);
   }
   /* Parsing done. Execute the command. */
   
@@ -367,6 +351,7 @@ void run_command(char* buf, int nbuf, int* pcp) {
         dup(p[1]);
         close(p[0]);
         close(p[1]);
+        
 
         if (exec(arguments[0], arguments) < 0) {
           fprintf(2, "exec failed: %s\n", arguments[0]);
@@ -394,7 +379,6 @@ void run_command(char* buf, int nbuf, int* pcp) {
       } else if (pid == 0) {
         close(pcp[0]);
         close(pcp[1]);
-        if (redirection_left || redirection_right) {}
         exec(arguments[0], arguments);
         fprintf(2, "exec failed: %s\n", arguments[0]);
         exit(0);
